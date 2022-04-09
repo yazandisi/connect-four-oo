9a227fe9d2ff0b0db68c65378bf78de582ac9a3a
Yazan Eldisi <yazandisi@gmail.com>
2022-04-08 20:09:45 -0500

    Part one and two done

---
+++ connect4.js
@@ -0,0 +1,164 @@
+class Game {
+  constructor(WIDTH, HEIGHT) {
+    this.WIDTH = WIDTH;
+    this.HEIGHT = HEIGHT;
+    this.currPlayer = 1;
+    this.board = [];
+    this.makeBoard();
+    this.makeHtmlBoard();
+    this.btnHolder = 0;
+  }
+  makeBoard() {
+    for (let y = 0; y < this.HEIGHT; y++) {
+      this.board.push(Array.from({ length: this.WIDTH }));
+    }
+  }
+  makeHtmlBoard() {
+    document.querySelectorAll("tr").forEach((val) => val.remove());
+    const board = document.getElementById("board");
+    let startBtn = document.createElement("button");
+    startBtn.innerText = "Start New Game";
+    board.append(startBtn);
+
+    const handleBtnClick = () => {
+      // make column tops (clickable area for adding a piece to that column)
+      this.currPlayer = 1;
+      for (let y = 0; y < this.HEIGHT; y++) {
+        this.board.pop(Array.from({ length: this.WIDTH }));
+      }
+      for (let y = 0; y < this.HEIGHT; y++) {
+        this.board.push(Array.from({ length: this.WIDTH }));
+      }
+      document.querySelectorAll("tr").forEach((val) => val.remove());
+
+      const top = document.createElement("tr");
+      top.setAttribute("id", "column-top");
+      top.addEventListener("click", this.handleClick.bind(this));
+
+      for (let x = 0; x < this.WIDTH; x++) {
+        const headCell = document.createElement("td");
+        headCell.setAttribute("id", x);
+        top.append(headCell);
+      }
+
+      board.append(top);
+
+      // make main part of board
+      for (let y = 0; y < this.HEIGHT; y++) {
+        const row = document.createElement("tr");
+
+        for (let x = 0; x < this.WIDTH; x++) {
+          const cell = document.createElement("td");
+          cell.setAttribute("id", `${y}-${x}`);
+          row.append(cell);
+        }
+
+        board.append(row);
+      }
+    };
+    startBtn.addEventListener("click", handleBtnClick);
+  }
+
+  findSpotForCol(x) {
+    for (let y = this.HEIGHT - 1; y >= 0; y--) {
+      if (!this.board[y][x]) {
+        return y;
+      }
+    }
+    return null;
+  }
+
+  placeInTable(y, x) {
+    const piece = document.createElement("div");
+    piece.classList.add("piece");
+    piece.classList.add(`p${this.currPlayer}`);
+    piece.style.top = -50 * (y + 2);
+
+    const spot = document.getElementById(`${y}-${x}`);
+    spot.append(piece);
+  }
+  endGame(msg) {
+    alert(msg);
+  }
+
+  handleClick(evt) {
+    // get x from ID of clicked cell
+    const x = +evt.target.id;
+
+    // get next spot in column (if none, ignore click)
+
+    const y = this.findSpotForCol(x);
+    if (y === null) {
+      return;
+    }
+
+    // place piece in board and add to HTML table
+    this.board[y][x] = this.currPlayer;
+    this.placeInTable(y, x);
+
+    // check for win
+    if (this.checkForWin()) {
+      document.querySelectorAll("#column-top").forEach((val) => val.remove());
+      return this.endGame(`Player ${this.currPlayer} won!`);
+    }
+
+    // check for tie
+    if (this.board.every((row) => row.every((cell) => cell))) {
+      document.querySelectorAll("#column-top").forEach((val) => val.remove());
+      return this.endGame("Tie!");
+    }
+
+    // switch players
+    this.currPlayer = this.currPlayer === 1 ? 2 : 1;
+  }
+
+  checkForWin() {
+    const _win = (cells) =>
+      cells.every(
+        ([y, x]) =>
+          y >= 0 &&
+          y < this.HEIGHT &&
+          x >= 0 &&
+          x < this.WIDTH &&
+          this.board[y][x] === this.currPlayer
+      );
+
+    for (let y = 0; y < this.HEIGHT; y++) {
+      for (let x = 0; x < this.WIDTH; x++) {
+        // get "check list" of 4 cells (starting here) for each of the different
+        // ways to win
+        const horiz = [
+          [y, x],
+          [y, x + 1],
+          [y, x + 2],
+          [y, x + 3],
+        ];
+        const vert = [
+          [y, x],
+          [y + 1, x],
+          [y + 2, x],
+          [y + 3, x],
+        ];
+        const diagDR = [
+          [y, x],
+          [y + 1, x + 1],
+          [y + 2, x + 2],
+          [y + 3, x + 3],
+        ];
+        const diagDL = [
+          [y, x],
+          [y + 1, x - 1],
+          [y + 2, x - 2],
+          [y + 3, x - 3],
+        ];
+
+        // find winner (only checking each win-possibility as needed)
+        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+          return true;
+        }
+      }
+    }
+  }
+}
+
+new Game(6, 7);
